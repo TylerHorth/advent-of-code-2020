182a2689bd37cb1abfa82d82b8ee1e25d5f3f68e
Tyler Horth <tylerhorth@outlook.com>
2020-12-14 23:43:40 -0800

    pre-allocate the array

--- src/day15.rs
+++ src/day15.rs
@@ -3,43 +3,36 @@ use std::num::ParseIntError;
 use aoc_runner_derive::{aoc, aoc_generator};
 
 #[aoc_generator(day15)]
-fn parse(input: &str) -> Result<Vec<usize>, ParseIntError> {
+fn parse(input: &str) -> Result<Vec<u32>, ParseIntError> {
     input.split(',').map(str::parse).collect()
 }
 
 struct IntMap {
-    data: Vec<usize>,
+    data: Vec<u32>,
 }
 
 impl IntMap {
-    const SENTINEL: usize = 0;
+    const SENTINEL: u32 = 0;
 
-    fn new() -> Self {
-        Self { data: Vec::new() }
+    fn with_capacity(capacity: u32) -> Self {
+        Self { data: vec![0; capacity as usize] }
     }
 
-    fn insert(&mut self, key: usize, value: usize) -> usize {
-        if key >= self.data.len() {
-            self.data.extend((self.data.len()..key).map(|_| Self::SENTINEL));
-            self.data.push(value);
-
-            Self::SENTINEL
-        } else {
-            std::mem::replace(&mut self.data[key], value)
-        }
+    fn insert(&mut self, key: u32, value: u32) -> u32 {
+        std::mem::replace(&mut self.data[key as usize], value)
     }
 }
 
-fn sequence_nth(seed: &[usize], n: usize) -> usize {
-    let mut seen = IntMap::new();
-    let mut turn = 0usize;
+fn sequence_nth(seed: &[u32], n: u32) -> u32 {
+    let mut seen = IntMap::with_capacity(n);
+    let mut turn = 0;
 
     for &i in seed {
         turn += 1;
         seen.insert(i, turn);
     }
 
-    let mut next = 0usize;
+    let mut next = 0;
 
     while turn < n - 1 {
         turn += 1;
@@ -55,11 +48,11 @@ fn sequence_nth(seed: &[usize], n: usize) -> usize {
 }
 
 #[aoc(day15, part1)]
-fn part1(seed: &[usize]) -> usize {
+fn part1(seed: &[u32]) -> u32 {
     sequence_nth(seed, 2020)
 }
 
 #[aoc(day15, part2)]
-fn part2(seed: &[usize]) -> usize {
+fn part2(seed: &[u32]) -> u32 {
     sequence_nth(seed, 30000000)
 }
